4e1518ccf3b3c03ffe5c57fb54858935e1b4ae0a
Naman <nj20@st-andrews.ac.uk>
2017-07-30 19:58:15 +0200

    Added restaurant endpoint

--- node/app.js
+++ node/app.js
@@ -4,9 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var restaurant = require('./routes/restaurant');
 
 var app = express();
 
@@ -22,11 +20,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api/v1/restaurant', restaurant);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req, res, next)
+{
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
--- node/routes/restaurant.js
+++ node/routes/restaurant.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var mongoDB = require("../db/mongoDB");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET users listing. */
+router.get('/', function(req, res, next)
+{
+    res.send('respond with a resource');
 });
 
 module.exports = router;
