1798da74336ced9b776216b500d4da2b0aab1eb6
Naman <nj20@st-andrews.ac.uk>
2017-07-30 12:40:59 +0200

    Start and stop script get environment variable from deployment group name

--- node/bin/start.sh
+++ node/bin/start.sh
@@ -4,26 +4,7 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${dir}
 
-#Parsing command line arguments
-while [[ $# -gt 1 ]]
-    do
-        key="$1"
-        case $key in
-            -e|--environment)
-            environment="$2"
-            shift # past argument
-            ;;
-        esac
-        shift # past argument or value
-    done
-
-if [[ -z ${environment} ]]
-    then
-        echo "Enter environment by -e"
-        exit 1
-fi
-
-source ../config/${environment}.env
+source ../config/${DEPLOYMENT_GROUP_NAME}.env
 
 cd ..
 npm install
