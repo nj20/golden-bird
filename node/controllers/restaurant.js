dae8f0c1526b10d2e5ff8b99af0c4fae54009548
nj20 <nj20@st-andrews.ac.uk>
2017-08-18 01:43:14 +0100

    Added endpoint to get all restaurants by location

--- node/controllers/restaurant.js
+++ node/controllers/restaurant.js
@@ -130,6 +130,50 @@ module.exports =
     },
 
     /**
+     * Returns a list of restaurants within the range of give lat and long
+     * @param long Longitude (Degrees)
+     * @param lat Latitude (Degrees)
+     * @param accuracy Accuracy of the give point (in meters)
+     * @param range Radius in meters
+     * @returns {Promise}
+     */
+    getRestaurantsByLocation: function(lat, long, accuracy, range)
+    {
+        return new Promise(function(fulfill, reject)
+        {
+            db.getAll(collection).then(function(result)
+            {
+                if(result.status == 200)
+                {
+                    result.body.toArray().then(function(restaurants)
+                    {
+                        var filteredRestaurants = [];
+                        for(var count = 0; count < restaurants.length; count++)
+                        {
+                            var distance = calculateDistance(lat, long, restaurants[count].latitude, restaurants[count].longitude);
+
+                            if(distance <= range)
+                            {
+                                restaurants[count].distance = distance;
+                                filteredRestaurants.push(restaurants[count]);
+                            }
+                        }
+                        fulfill(
+                        {
+                            status: 200,
+                            body: filteredRestaurants
+                        })
+                    });
+                }
+                else
+                {
+                    fulfill(result);
+                }
+            });
+        });
+    },
+
+    /**
      *
      * @param {Object} user
      * @param {string} restaurantId
@@ -212,4 +256,25 @@ module.exports =
     {
         return collection;
     }
+}
+
+var calculateDistance = function(lat1, lon1, lat2, lon2)
+{
+    var R = 6371e3; // metres
+    var φ1 = toRadians(lat1);
+    var φ2 = toRadians(lat2);
+    var Δφ = toRadians(lat2-lat1);
+    var Δλ = toRadians(lon2-lon1);
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return R * c;
+}
+
+var toRadians = function(degrees)
+{
+    return degrees * (Math.PI / 180);
 }
--- node/routes/restaurant.js
+++ node/routes/restaurant.js
@@ -8,6 +8,7 @@ var restaurantController = require("../controllers/restaurant");
 var userController = require("../controllers/user");
 var multer = require("multer");
 var router = express.Router();
+var verifyHeaders = require("../util/verifyHeaders");
 
 restaurantController.setDB(db);
 userController.setDB(db);
@@ -80,6 +81,48 @@ router.get('/allForCustomer', function(req, res, next)
                 "body": result.body
             });
         });
+    }
+    else
+    {
+        res.statusCode = 401;
+        res.json(
+        {
+            "body": "Not Authorized. Please provide valid sessionKey in headers"
+        });
+    }
+});
+
+/**
+ * Gets all restaurants of customer.
+ */
+router.get('/location/all', function(req, res, next)
+{
+    if(req.headers.authStatus == 200)
+    {
+        if(verifyHeaders(req, ["latitude", "longitude", "accuracy", "range"]))
+        {
+            var lat = req.headers["latitude"];
+            var long = req.headers["longitude"];
+            var accuracy = req.headers["accuracy"];
+            var range = req.headers["range"];
+
+            restaurantController.getRestaurantsByLocation(lat, long, accuracy, range).then(function(result)
+            {
+                res.statusCode = result.status;
+                res.json(
+                {
+                    "body": result.body
+                });
+            });
+        }
+        else
+        {
+            res.statusCode = 400;
+            res.json(
+            {
+                "body": "Missing one of the headers: 'latitude', 'longitude', 'accuracy', 'range'"
+            });
+        }
     }
     else
     {
